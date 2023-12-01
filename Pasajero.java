c9613de22d9078fc009168cec076674c4e79d530
KevinGuanoluisa <kmguanoluisa1@espe.edu.ec>
2023-12-01 18:46:41 -0500

    leccion subida con exito

---
+++ Pasajero.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebakevin;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Pasajero extends Persona{
+    String numID;
+
+    public Pasajero(String numID, String nombre, int edad) {
+        super(nombre, edad);
+        this.numID = numID;
+    }
+    
+     public void pedirtaxi (){
+        System.out.println( "Esta solicitando un taxi ....");
+    }
+    public void cancelartaxi(){
+        System.out.println( "Esta cancelando el taxi ...");
+    }
+    
+}
+
---
+++ Pruebakevin.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebakevin;
+
+/**
+ *
+ * @author Bryan
+ */
+import java.util.Scanner;
+
+public class Pruebakevin {
+
+    public static void main(String[] args) {
+       
+       Scanner scanner = new Scanner (System.in);
+       Pasajero pasajero = new Pasajero ("234","Bryan",123);
+       Chofer chofer = new Chofer("2610" , "312" ,"Juan",30);
+       
+   
+        System.out.println(" 1 Para Persona");       
+        System.out.println("2 Para pasajero");
+        System.out.println("3 para chofer");
+       
+        switch(scanner.nextInt()){
+          
+            case 1:
+              System.out.println("ingrese el nombre de la persona");
+              String nombrePersona = scanner.next();
+               System.out.println("Ingrese la edad de la persona: ");
+               int edadPersona = scanner.nextInt();
+               System.out.println("Ingrese la cedula de la persona");
+               String cedulaPersona = scanner.next();
+              scanner.nextLine();
+              break;
+              
+            case 2 : 
+                System.out.println("La accion del pasajero es: ");
+               pasajero.pedirtaxi();
+               pasajero.cancelartaxi();
+               scanner.nextLine(); //limpiar buffer
+               break;
+         
+            case 3:
+                System.out.println("La accion realizada por el chofer es: ");
+                chofer.concelarcarrera();
+                chofer.aceptarcarrera();
+                scanner.nextLine();
+                break; 
+             
+        }
+    }    
+ }
